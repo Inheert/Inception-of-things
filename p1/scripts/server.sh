070fc0b866ac45d730fb3384997dcf8841035136
Inheert <theoclaereboudt@gmail.com>
2026-04-24 03:51:40 +0200

    kubectl added

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -11,4 +11,8 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -
 
 cat /var/lib/rancher/k3s/server/node-token > /vagrant/node-token.tmp
 
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
 echo "Server script has been successfully executed."
