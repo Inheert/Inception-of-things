d2daf61cbb0bfc9ff3e8de485f9d1f40c6f8e941
Inheert <theoclaereboudt@gmail.com>
2026-04-24 21:30:26 +0200

    documentations

--- p1/scripts/agent.sh
+++ p1/scripts/agent.sh
@@ -6,6 +6,8 @@ apt-get update
 
 apt-get install curl -y
 
+# Install k3s and set the VM as worker using K3S_URL, K3S_TOKEN should be equal to the token in the control
+# server at /var/lib/rancher/k3s/server/node-token.
 curl -sfL https://get.k3s.io | K3S_URL="https://192.168.56.110:6443" K3S_TOKEN=$(cat /vagrant/node-token.tmp) sh -
 
 echo "Agent script has been successfully executed."
--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
+# store outputs of the script in the specified file
 exec > /var/log/provision.log 2>&1
-# set -x
 
 apt-get update
 
 apt-get install curl -y
 
+# Install k3s in server mode, INSTALL_K3S_EXEC="server" is optional, if K3S_URL is not set then the VM is considered
+# as a control server.
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -
 
+# Used to share the control server's token to the worker, dont forget to delete the file once everything is set.
 cat /var/lib/rancher/k3s/server/node-token > /vagrant/node-token.tmp
-
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 echo "Server script has been successfully executed."
