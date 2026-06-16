413ccf567f5eefa7709a943a396b9c223fc5f0e8
Inheert <theoclaereboudt@gmail.com>
2026-06-16 03:04:11 +0200

    blabla

--- p3/scripts/cleaning.sh
+++ p3/scripts/cleaning.sh
@@ -6,9 +6,9 @@ if kubectl delete application my-app -n argocd > /dev/null 2>&1; then
 fi
 
 kubectl scale deployment -n dev --replicas=0 --all
-kubectl scale deployment --replicas=0 --all
+# kubectl scale deployment --replicas=0 --all
 
 if kill $(lsof -i tcp:4852 | tail -n +2 | awk '{print $2}') > /dev/null 2>&1; then
 	echo "Port-forwarding stopped."
 fi
-# k3d cluster delete $1
+k3d cluster delete $1
--- p3/scripts/producer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Output of this script is displayed inside the box in display.sh.
-# Replace or extend with whatever you want to show.
-
